ab3afc7d3c9186bdb86ee8995ba4931e60eda937
Jean-Marie Pivette <jpivette@gmail.com>
2019-10-28 21:48:17 +0100

    deleting unfinished tidytuesday

--- nobel_prize/nobel_prize.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-
-nobel_winners <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winners.csv")
-nobel_winner_all_pubs <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-14/nobel_winner_all_pubs.csv")
-
